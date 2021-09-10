359cca5f4216abf12795381ff68dd1464cec90a3
Shah-Arsalan <shaharslan2278@gmail.com>
2021-09-11 04:41:44 +0530

    added bonuses

--- stock.js
+++ stock.js
@@ -6,7 +6,19 @@ var outputDiv = document.querySelector("#output")
 
 button.addEventListener("click", calculate);
 
+
+
 function calculate(){
+
+    if(initialPrice.value ===""){
+        alert("Please fill all feilds")
+    }else if(Quantity.value === ""){
+        alert("Please fill all fields")
+    }else if(currentPrice.value === ""){
+        alert("Please fill all fields")
+    }
+else{
+
     var costPrice = (initialPrice.value *Quantity.value);
     var sellingPrice = (currentPrice.value *Quantity.value);
     //console.log(costPrice);
@@ -16,15 +28,19 @@ function calculate(){
         var profit = sellingPrice - costPrice;
         var profitPercentage = (profit/costPrice)*100;
         //console.log(profit.value)
-        outputDiv.innerText = "It's a profit of "+ profit + " and the profit percentage is " + profitPercentage + " percent";
+        outputDiv.innerText = "It's a profit of "+ profit + " and the profit percentage is " + profitPercentage.toFixed(2) + " percent";
+        outputDiv.style.color = "green";
     }
     else if(costPrice > sellingPrice){
         var loss =  costPrice - sellingPrice ;
         var lossPercentage = (loss/costPrice)*100;
         //console.log(profit.value)
-        outputDiv.innerText = "It's a loss of "+ loss + " and the loss percentage is " + lossPercentage + " percent.";
+        outputDiv.innerText = "It's a loss of "+ loss + " and the loss percentage is " + lossPercentage.toFixed(2) + " percent.";
+        outputDiv.style.color = "red";
+        
     }
     else{
         outputDiv.innerText = "The Stock's value has remained the same . No loss or profit"
     }
+}
 }
